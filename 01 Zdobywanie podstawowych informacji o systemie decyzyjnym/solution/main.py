67f16c10623fb58a21b7234cc0d4b3a5ac8456c4
Kamil Smulski <kamilsmulski123@gmail.com>
2019-11-26 21:21:09 +0100

    WIP: add draft of data loading

--- 01 Zdobywanie podstawowych informacji o systemie decyzyjnym/solution/main.py
+++ 01 Zdobywanie podstawowych informacji o systemie decyzyjnym/solution/main.py
@@ -12,18 +12,44 @@ def load_metadata():
         lines = file.readlines()
 
     for line in lines:
-        metadata = SystemMetadata(line.split()[0], line.split()[1], line.split()[2])
-        metadata.print()
+        system = System(line.split()[0], line.split()[1], line.split()[2])
+        system.load_data()
+        print("--------------------------------------------")
+        system.print_info()
 
 
-class SystemMetadata:
+class System:
     def __init__(self, name, nr_attr, nr_val):
+        print(f"Creating system {name}!")
         self.name = name
-        self.number_of_attributes = nr_attr
+        self.number_of_attributes = int(nr_attr)
         self.number_of_values = nr_val
+        self.data = []
+        self.columns = []
+        for i in range(self.number_of_attributes):
+            self.columns.append([])
 
-    def print(self):
-        print(self.name, self.number_of_attributes, self.number_of_values)
+    def print_info(self):
+        pass
+
+    def load_data(self):
+        with open(DATA_DIR + "/" + self.name + ".txt") as file:
+            for line in file:
+                self.data.append(line.strip().split(" "))
+
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                self.columns[j].append(self.data[i][j])
+
+
+class DecisionClass:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.values = []
+
+    def load_values(self, data, i):
+        pass
 
 
 if __name__ == '__main__':
